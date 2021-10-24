f6b330e77a89847a0a1304d0fdf383b860f62e47
billy93 <billyfebram@gmail.com>
2021-10-24 14:42:16 +0700

    Initial commit

---
+++ precache-manifest.afd46a70a1b317c0f6faf7fd2a3119cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27e3b5d28aa112aa2cd89d9664e3158f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "52500423a69eaf6a17f9",
+    "url": "./static/css/2.f7861fd5.chunk.css"
+  },
+  {
+    "revision": "0f642e5eb86ff95d6e9b",
+    "url": "./static/css/main.836ecd7b.chunk.css"
+  },
+  {
+    "revision": "52500423a69eaf6a17f9",
+    "url": "./static/js/2.c3582dd8.chunk.js"
+  },
+  {
+    "revision": "d948b697ef2e61857feb5add88694b92",
+    "url": "./static/js/2.c3582dd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f642e5eb86ff95d6e9b",
+    "url": "./static/js/main.84486f0d.chunk.js"
+  },
+  {
+    "revision": "028762adaf48b66839f6",
+    "url": "./static/js/runtime-main.08baa623.js"
+  }
+]);
